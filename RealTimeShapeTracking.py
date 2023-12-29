24d2c1b10a5b158f2db876540a9eb1f8179eddd0
izalfront <rizalhajakubastarus@gmail.com>
2023-12-29 23:02:03 +0800

    ShapeDetection

--- RealTimeShapeTracking.py
+++ RealTimeShapeTracking.py
@@ -9,12 +9,13 @@ def nothing(x):
 cap = cv2.VideoCapture(0)
 
 cv2.namedWindow('Trackbars')
-cv2.createTrackbar('L-H','Trackbars', 0, 180, nothing)
-cv2.createTrackbar('L-S','Trackbars', 66, 255, nothing)
-cv2.createTrackbar('L-V','Trackbars', 134, 255, nothing)
-cv2.createTrackbar('U-H','Trackbars', 180, 180, nothing)
-cv2.createTrackbar('U-S','Trackbars', 255, 255, nothing)
-cv2.createTrackbar('U-V','Trackbars', 243, 255, nothing)
+cv2.createTrackbar('L-H', 'Trackbars', 0, 180, nothing)
+cv2.createTrackbar('L-S', 'Trackbars', 66, 255, nothing)
+cv2.createTrackbar('L-V', 'Trackbars', 134, 255, nothing)
+cv2.createTrackbar('U-H', 'Trackbars', 180, 180, nothing)
+cv2.createTrackbar('U-S', 'Trackbars', 255, 255, nothing)
+cv2.createTrackbar('U-V', 'Trackbars', 243, 255, nothing)
+
 
 font = cv2.FONT_HERSHEY_COMPLEX
 
@@ -22,12 +23,13 @@ while True:
     _, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    l_h = cv2.getTrackbarPos('L-H','Trackbars')
-    l_s = cv2.getTrackbarPos('L-S','Trackbars')
-    l_v = cv2.getTrackbarPos('L-H','Trackbars')
-    u_h = cv2.getTrackbarPos('L-H','Trackbars')
-    u_s = cv2.getTrackbarPos('L-H','Trackbars')
-    u_v = cv2.getTrackbarPos('L-H','Trackbars')
+    l_h = cv2.getTrackbarPos('L-H', 'Trackbars')
+    l_s = cv2.getTrackbarPos('L-S', 'Trackbars')
+    l_v = cv2.getTrackbarPos('L-V', 'Trackbars')
+    u_h = cv2.getTrackbarPos('U-H', 'Trackbars')
+    u_s = cv2.getTrackbarPos('U-S', 'Trackbars')
+    u_v = cv2.getTrackbarPos('U-V', 'Trackbars')
+
     
     lower_red = np.array([l_h,l_s,l_v])
     upper_red = np.array([u_h,u_s,u_v])
@@ -37,10 +39,11 @@ while True:
     mask = cv2.erode(mask,kernel)
 
     #Deteksi Contours
-    if int(cv2.__version__[0]) > 3:
-        contours,_ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    if cv2.__version__.startswith('4'):
+       contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     else:
-        _,contours,_ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+       _, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
 
     for cnt in contours:
         area = cv2.contourArea(cnt)
